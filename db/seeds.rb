1a24b7daba418a4750f9a882898e9370f13622c0
finn1990 <fall1600@gmail.com>
2015-07-15 15:54:09 +0800

    seed 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-post1 = Post.create(title: "public post", content: "content of public post", status: "public", user_id: 1)
-post2 = Post.create(title: "private post", content: "content of private post", status: "private", user_id: 1)
-post3 = Post.create(title: "deleted post", content: "content of deleted post", status: "deleted", user_id: 1)
-
 guest = User.create(id: 1, name: "guest", email: "guest@demo.com", about: "for guest", password: "1234", password_confirmation: "1234")
+
+user1 = User.create(id: 2, name: "fall1600", email: "fall1600@demo.com", about: "developer", password: "fjdkfjdk", password_confirmation: "fjdkfjdk")
+
+post1 = Post.create(title: "public post", content: "content of public post", status: "public", user_id: 2)
+post2 = Post.create(title: "private post", content: "content of private post", status: "private", user_id: 2)
+post3 = Post.create(title: "deleted post", content: "content of deleted post", status: "deleted", user_id: 2)
+
