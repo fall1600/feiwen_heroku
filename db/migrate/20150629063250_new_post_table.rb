321e010f16630a9917189789d9e8f94aac60c2cc
finn1990 <fall1600@gmail.com>
2015-07-15 15:28:33 +0800

    first

--- db/migrate/20150629063250_new_post_table.rb
+++
@@ -1,9 +0,0 @@
-class NewPostTable < ActiveRecord::Migration
-  def change
-    create_table :posts do |t|
-      t.string :title
-      t.string :content
-      t.integer :views
-    end
-  end
-end
