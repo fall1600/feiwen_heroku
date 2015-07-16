5dbe1e26b230b7546d5bef69df881384a0560abf
finn1990 <fall1600@gmail.com>
2015-07-16 16:58:27 +0800

    3 bootstrap

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -1,6 +1,6 @@
 module NavbarHelper
   def nav_li text, path
-    active = request.path == path ? :active : nil
-    content_tag :li, link_to(text, path), class: active
+    active = request.path == path ? "active" : ""
+    content_tag :nav, link_to(text, path), class: active + " navbar-nav"
   end
 end
